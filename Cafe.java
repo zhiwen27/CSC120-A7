4cbc8c42534ee6f38ca1b73d58e2b15ec8b7dbc6
Zhiwen Ji <zji@smith.edu>
2024-04-02 12:04:42 -0400

    Overloads in cafe

--- Cafe.java
+++ Cafe.java
@@ -9,7 +9,7 @@ public class Cafe extends Building{
     /** Default Constructor */
     public Cafe(){
         this("<Name Unknown>", "<Address Unknown>", 1);
-        this.restock(100, 100, 100, 100);
+        this.restock();
     }
 
     /**
@@ -40,15 +40,43 @@ public class Cafe extends Building{
     }
 
     /**
-     * Decrease the remaining inventory after selling a cup of coffee
+     * Default method: Decrease the remaining inventory after selling a cup of coffee
+     * Sell coffee of size 3, 2 packets of sugar, 1 splash of cream
+     */
+    public void sellCoffee(){
+        this.sellCoffee(3,2,1);
+    }
+
+    /**
+     * No sugar and no cream: Decrease the remaining inventory after selling a cup of coffee
+     * @param size the size of coffee
+     */
+    public void sellCoffee(int size){
+        this.sellCoffee(size,0,0);
+    }
+
+    /**
+     * No sugar: Decrease the remaining inventory after selling a cup of coffee
+     * @param size the size of coffee
+     * @param nCreams the number of sugar packets required
+     */
+    public void sellCoffee(int size, int nCreams){
+        this.sellCoffee(size, 0, nCreams);
+    }
+
+    /**
+     * Full method: Decrease the remaining inventory after selling a cup of coffee
      * @param size the size of coffee
      * @param nSugarPackets the number of sugar packets required
      * @param nCreams cream required
      */
     public void sellCoffee(int size, int nSugarPackets, int nCreams){
         if ((this.nCoffeeOunces < size) || (this.nSugarPackets < nSugarPackets) || (this.nCreams < nCreams) || (this.nCups < 1)){
-            System.out.println("There aren't enough inventories... need restock!");
-            restock(size - this.nCoffeeOunces, nSugarPackets - this.nSugarPackets, nCreams - this.nCreams, 1);
+            System.out.println("There aren't enough inventories... need emergency restock!");
+            this.restock(size - this.nCoffeeOunces, nSugarPackets - this.nSugarPackets, nCreams - this.nCreams, 1);
+            this.sellCoffee(size,nSugarPackets,nCreams);
+            System.out.println("Normal restock.");
+            this.restock();
         }
         else{
             this.nCoffeeOunces -= size;
@@ -59,7 +87,14 @@ public class Cafe extends Building{
     }
 
     /**
-     * Restock the inventory of the cafe
+     * Default method: Restock the inventory of the cafe
+     * Restock 100 ounces of coffee, 100 packets of sugar, 100 splashes of creams and 10 cups
+     */
+    public void restock(){
+        this.restock(100,100,100,10);
+    }
+    /**
+     * Full method: Restock the inventory of the cafe
      * @param nCoffeeOunces the number of ounces of coffee added to the supplement
      * @param nSugarPackets the number of sugar packets added to the supplement
      * @param nCreams cream added to the supplement
@@ -102,10 +137,12 @@ public class Cafe extends Building{
     public static void main(String[] args) {
         Cafe campusCenter = new Cafe("Campus Center Cafe", "100 Elm Street Northampton, MA 01063", 2);
         System.out.println(campusCenter);
-        campusCenter.sellCoffee(2, 1, 1);
+        campusCenter.sellCoffee(101, 102, 103);
+        System.out.println(campusCenter);
+        campusCenter.sellCoffee();
         System.out.println(campusCenter);
         campusCenter.enter();
-        campusCenter.showOptions();
-        campusCenter.goToFloor(1);
+        //campusCenter.showOptions();
+        //campusCenter.goToFloor(1);
     }
 }
